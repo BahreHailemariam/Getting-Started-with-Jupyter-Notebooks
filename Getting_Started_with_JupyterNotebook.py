7ab047ce4c86ec83cacb3032dee430fafe0ddc4a
BahreHailemariam <134759166+BahreHailemariam@users.noreply.github.com>
2025-05-22 13:12:49 -0700

    Create Getting_Started_with_JupyterNotebook.py

---
+++ Getting_Started_with_JupyterNotebook.py
@@ -0,0 +1,8 @@
+#Objectives
+##After completing this lab, you will be able to:
+
+#### Get an overview of code and markdown cells
+#### Execute an existing code cell
+#### Insert and delete a code cell
+#### Write comments in Python
+#### Create and use Markdown cells
